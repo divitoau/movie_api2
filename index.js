dcba43620cfa88f2591c23dd5f5858ef8f436f63
divitoau <divitoau@gmail.com>
2023-07-17 02:03:18 -0400

    re-added commented out local db

--- index.js
+++ index.js
@@ -49,6 +49,11 @@ let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
+/* mongoose.connect("mongodb://127.0.0.1:27017/cfDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); */
+
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
