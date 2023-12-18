afbc6c2ec9175660e97288c42d16b2e63591748c
divitoau <divitoau@gmail.com>
2023-12-17 21:03:02 -0500

    added dotenv

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const app = express();
 const { check, validationResult } = require("express-validator");
+require("dotenv").config();
 const {
   S3Client,
   ListObjectsV2Command,
